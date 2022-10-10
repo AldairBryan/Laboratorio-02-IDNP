dc9652e5aa36796038f300b5eafc6a847f91ac33
AldairBryan <asalcedoch@unsa.edu.pe>
2022-10-10 11:13:06 -0500

    Ingresando Codigo

--- app/src/main/java/com/example/laboratorio_02/MainActivity.java
+++ app/src/main/java/com/example/laboratorio_02/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText apellidoPaterno =findViewById(R.id.inputApellidoP);
+        EditText apellidoMaterno =findViewById(R.id.inputApellidoM);
+        EditText nombres =findViewById(R.id.inputNombres);
+        EditText fechaNacimiento =findViewById(R.id.inputFechaNacimiento);
+        EditText codigoProcedencia =findViewById(R.id.inputCodigoP);
+        Spinner carreraPostular =findViewById(R.id.inputCarrera);
+
     }
 }
